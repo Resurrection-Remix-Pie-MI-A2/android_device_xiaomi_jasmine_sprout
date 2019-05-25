818736106c470b33a69962ae6d85b3633cfd972a
Manish4586 <manish.n.manish45@gmail.com>
2019-05-26 01:33:57 +0530

    jasmine_sprout: update fingerprint with google taimen

--- rr_jasmine_sprout.mk
+++ rr_jasmine_sprout.mk
@@ -36,8 +36,8 @@ PRODUCT_MODEL := Mi A2
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="jasmine_sprout" \
     PRODUCT_NAME="jasmine" \
-    PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.3.0.PDIMIXM release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ2A.190405.003 5310204 release-keys"
 
-BUILD_FINGERPRINT := "xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.3.0.PDIMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/taimen/taimen:9/PQ2A.190405.003/5310204:user/release-keys"
 
 TARGET_VENDOR_PRODUCT_NAME := jasmine_sprout
